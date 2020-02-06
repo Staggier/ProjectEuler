7b8d5465098aed24801a10db11dc6a63b35bdf6e
Jordan McIntyre <39545402+Staggier@users.noreply.github.com>
2020-02-05 20:58:22 -0500

    Update PE#057.rkt
    Rewrote the solution to Project Euler #057, it looks much nicer than the previous version.

--- PE#057.rkt
+++ PE#057.rkt
@@ -1,14 +1,10 @@
-;; The Problem can be found here: https://projecteuler.net/problem=57
-(define (sqrtConvergents exp n d)
-  (if (= exp 2)
-      0
-      (if (<= (+ (floor n) 0.5) n)
-          (if (> (string-length (number->string (inexact->exact (+ (floor n) 1)))) (string-length (number->string d)))
-              (+ 1 (sqrtConvergents (- exp 1) (/ (* (+ (+ (floor n) 1) d) (+ (floor n) 1)) d) (+ (inexact->exact (+ (floor n) 1)) d)))
-                   (sqrtConvergents (- exp 1) (/ (* (+ (+ (floor n) 1) d) (+ (floor n) 1)) d) (+ (inexact->exact (+ (floor n) 1)) d)))
-          (if (> (string-length (number->string (inexact->exact (floor n)))) (string-length (number->string d)))
-              (+ 1 (sqrtConvergents (- exp 1) (/ (* (+ (floor n) d) (floor n)) d) (+ (inexact->exact (floor n)) d)))
-                   (sqrtConvergents (- exp 1) (/ (* (+ (floor n) d) (floor n)) d) (+ (inexact->exact (floor n)) d))))))
+;; The Problem can be found here: projecteuler.net/problem=57
 
-(sqrtConvergents 1000 7 5)
-;; Had to begin with the 2nd expansion otherwise all values afterward would be inaccurate from the cross multiplication
+(define (sqrtConvergents num n d)
+  (if (= num 0)
+      0
+      (if (> (string-length (number->string n)) (string-length (number->string d)))
+          (+ 1 (sqrtConvergents (- num 1) (round (/ (* n (+ n d)) d)) (+ n d)))
+          (sqrtConvergents (- num 1) (round (/ (* n (+ n d)) d)) (+ n d)))))
+
+(sqrtConvergents 998 7 5)
