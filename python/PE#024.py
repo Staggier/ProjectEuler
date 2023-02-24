036667e6ea0ca523caf15eca6f70e3ca63a8618f
Jordan McIntyre <39545402+Staggier@users.noreply.github.com>
2023-02-24 00:42:36 -0500

    Added comments and modified code

--- python/PE#024.py
+++ python/PE#024.py
@@ -1,34 +1,39 @@
 #The problem can be found here: https://projecteuler.net/problem=24
 
-def sf(n):
-    i, t = 0, 0
-    while f(t) < n:
-        t += 1
-    return t - 1
+# Gets largest factorial(i) < n and returns i
+def sf(n: int) -> int:
+    i = 0
+    while f(i) < n:
+        i += 1
+    return i - 1
 
-def f(n):
+# Factorial
+def f(n: int) -> int:
     product = 1
     for i in range(2, n + 1):
         product *= i
     return product
 
-def lf(n, k):
+# Gets the largest f(k) * i < n and returns the sum and i
+def lf(n: int, k: int) -> list[int, int]: 
     total = 0
-    c = 0
+    i = 0
     while total < n:
         total += f(k)
-        c += 1
+        i += 1
     
-    return [total - f(k), c - 1]
+    return [total - f(k), i - 2]
 
-def lp(n):
+# Gets the n-th lexicographic permutation of digits 0-9
+def lp(n: int) -> int:
     f = sf(n)
     ans = ""
 
+    #list of digits to be removed
     dlst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+    
     while f >= 0:
-        tlst = lf(n, f)
-        t, d = tlst[0], tlst[1] - 1
+        t, d = lf(n, f)
         
         n -= t
         ans += str(dlst[d])
@@ -37,7 +42,7 @@ def lp(n):
         
     return ans
 
-print(lp(1000000))
+print(lp(10 ** 6))
 
 # Each digit can occur 9! times, the n! going down by one after each time a digit is removed.
 # The max amount of times f(n) * i that can fit in n (minus 1) is the next digit.
